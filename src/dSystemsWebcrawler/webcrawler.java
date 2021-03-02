5a3d492bf1660a176173f979a84d75484cd0a91c
Benjamin Rafter <brafter@outlook.com>
2021-03-01 19:16:25 -0600

    Table fully parsed

--- src/dSystemsWebcrawler/webcrawler.java
+++ src/dSystemsWebcrawler/webcrawler.java
@@ -11,21 +11,15 @@ import org.jsoup.select.Elements;
 
 public class webcrawler {
 	
-	public static void print(String ret) {
+	public static void print(Object ret) {
 		System.out.println(ret);
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("Running....");
-		String url = args[0];
-		System.out.println("Starting page: " + url);
-		System.out.println("Domain prefix: " + args[1]);
-		System.out.println("Link amount: " + args[2]);
+	public static void parseTable(String url) {
 		Document document;
 		try {
 			document = Jsoup.connect(url).timeout(10000).get();
 			String title = document.title();
-			System.out.println("Title: " + title);
 			
 			Element table = document.select("table.wikitable").first();
 			
@@ -37,14 +31,29 @@ public class webcrawler {
 			
 			Elements tableRows = table.select("tr");
 			
-			for(int i = 0; i < tableRows.size(); i++) {
+			for(int i = 1; i < tableRows.size(); i++) {
 				Element row = tableRows.get(i);
 				Elements cols = row.select("td");
-				
-				
+				print(cols.get(0).text());
+				print(cols.get(1).text());
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Running....");
+		String url = args[0];
+			
+		int doNums = Integer.parseInt(args[2]);
+		
+		int count = 0;
+		while(doNums >= 0) {
+			parseTable(url);
+			count++;
+			doNums--;
+			print(count + " tables parsed");
 		}
 		
 		System.out.println("Done");
